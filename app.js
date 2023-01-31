44429dea1ddc17b4201bb194d2017c67b5d235b1
rnk0085 <rinawkmbi4k8@gmail.com>
2023-01-31 22:15:18 +0900

    feat: top.ejs を活用するように修正

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 // https://www.sejuku.net/blog/74845
 // https://zenn.dev/oreo2990/articles/b4719c78aa0832
 const express = require('express');
+const path = require('path');
+const ejsMate = require('ejs-mate');
 
 const app = express();
+
+app.engine('ejs', ejsMate);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 app.listen(3000, () => {
     console.log('ポート3000でリクエスト待受中...');
 });
 
 app.get('/', (req, res) => {
-    res.send("Hello World");
+    res.render('top');
 });
